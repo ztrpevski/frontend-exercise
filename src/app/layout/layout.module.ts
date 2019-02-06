06e0408402a9284be3b4a0d76f91fec14a7e8ff0
ztrpevski <ztrpevski@wcc-group.com>
2019-02-06 16:04:09 +0100

    #2 added some static components for header sidebar and footer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,12 @@ import {CommonModule} from '@angular/common';
 import {MainComponent} from './main/main.component';
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "../app-routing.module";
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, HeaderComponent, SidebarComponent, FooterComponent],
   imports: [
     CommonModule,
     RouterModule,
