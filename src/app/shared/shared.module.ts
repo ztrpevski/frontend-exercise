14c943a73b678c6990601005e9335ea1ec4ed900
ztrpevski <ztrpevski@wcc-group.com>
2019-02-12 16:00:22 +0100

    #4 installed scrollbar component
    make the search items scroller to be the same in all browsers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SearchComponent} from './search/search.component';
+import {NgScrollbarModule} from "ngx-scrollbar";
 
 @NgModule({
   declarations: [
     SearchComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgScrollbarModule
   ],
   exports: [
     SearchComponent
